7d36167ff83640d7ddea65b541b2e702e63a0dff
draqyang <draqyang@coevo.com.tw>
2013-09-18 17:15:33 +0800

    [add] init.

---
+++ main/admin.py
@@ -0,0 +1,36 @@
+﻿#-*- encoding: utf-8 -*-
+from django.db import transaction
+from django.db import models 
+
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+from django.contrib import admin
+from django.contrib.auth.models import User 
+from django.contrib.auth.admin import UserAdmin 
+from django.contrib.auth.forms import AdminPasswordChangeForm, UserCreationForm
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.csrf import csrf_protect
+
+from main.models import *
+
+csrf_protect_m = method_decorator(csrf_protect)
+
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+    verbose_name_plural = u'會員'
+    
+class UserAdmin(UserAdmin):
+    inlines = (MemberInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Member)
---
+++ main/models.py
@@ -0,0 +1,29 @@
+﻿from django.db import models
+
+from mezzanine.core.fields import FileField, RichTextField
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+# Create your models here.
+
+class ContentBaseModel(models.Model):
+    name =  models.CharField(_(u"標題"), max_length=255)
+    content = RichTextField(_(u"內容"))
+    class Meta:
+        abstract = True
+        
+class TimeBaseModel(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+class Member(TimeBaseModel):
+    user = models.OneToOneField(User)
+    address = models.CharField(_(u"送貨地址"), max_length=255)
+    telphone = models.CharField(_(u"電話"), max_length=255)
+    cellphone = models.CharField(_(u"手機"), max_length=255)
+    
+    def __unicode__(self):
+        return user
